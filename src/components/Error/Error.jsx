c5f520c046ca231772bdfb65aaa8c3d95e2ea8df
samybensalem7 <samybenss09@gmail.com>
2025-11-23 15:11:22 +0100

    page d'erreur terminée

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,8 +1,18 @@
+import "./Error.scss"
+import { Link } from "react-router-dom";
+
+
+
+
 function Error(){
     return (
-        <div>
-            <h1>Oups cette page n'existe pas </h1>
+        <div className="error">
+            <p className="error__number">404</p>
+            <p className="error__message">Oups! La page que vous demandez n'existe pas.</p> 
+            <Link to="/" className="return__link">Retourner sur la page d'accueil</Link>
         </div>
+       
+       
     )
 }
 
