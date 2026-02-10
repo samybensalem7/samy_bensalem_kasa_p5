1787aa45b81a0b29c1059317a73ce21711107694
samybensalem7 <samybenss09@gmail.com>
2026-02-10 15:21:31 +0100

    ajouts rating et collapse

--- src/components/Slideshow/Slideshow.jsx
+++ src/components/Slideshow/Slideshow.jsx
@@ -8,7 +8,7 @@ import chevronRight from "../../assets/chevronRight.png"
 function Slideshow({pictures}){
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    const handleMinus = () => { setCurrentIndex( prev => prev === 0 ? pictures.length - 1 : prev - 1)}
+    const handleMinus = () => { setCurrentIndex( prev => prev === 0 ? pictures.length - 1 : prev - 1)} // à revoir
     const handlePlus = () => {setCurrentIndex(prev => prev === pictures.length - 1 ? 0 : prev + 1 )}
 
 
@@ -16,8 +16,8 @@ function Slideshow({pictures}){
 
 
     return(
-        <div>
-            {pictures.length > 1 ? <div> <img src={chevronLeft} onClick={handleMinus} className="chevron-left"/> <img className="img-pictures" src={pictures[currentIndex]}/> <img src={chevronRight} onClick={handlePlus} className="chevron-right"/> <span>{currentIndex + 1} / {pictures.length}</span> </div> : <img src={pictures[currentIndex]}/>}
+        <div className="slideshow-container ">
+            {pictures.length > 1 ? <div> <img src={chevronLeft} onClick={handleMinus} className="chevron-left"/> <img className="img-pictures" src={pictures[currentIndex]}/> <img src={chevronRight} onClick={handlePlus} className="chevron-right"/> <span>{currentIndex + 1} / {pictures.length}</span> </div> : <img className="img-pictures" src={pictures[currentIndex]}/>}
             
         </div>
     )
--- src/pages/Logement/Logement.jsx
+++ src/pages/Logement/Logement.jsx
@@ -2,17 +2,47 @@ import { useParams } from "react-router-dom";
 import Slideshow from "../../components/Slideshow/Slideshow";
 import logements from "../../data/logements.json";
 import "./logement.scss";
+import starR from "../../assets/starR.png";
+import starW from "../../assets/starW.png";
+import Collapse from "../../components/Collapse/Collapse";
 
 function Logement() {
   const { id } = useParams();
 
   const logementActive = logements.find((l) => l.id === id);
 
+  const number = [1, 2, 3, 4, 5];
+
   return (
     <div className="logements-container">
       <Slideshow pictures={logementActive.pictures} />
-      <h3> {logementActive.title}</h3>
-      <h4>{logementActive.location}</h4>
+      <div className="logements-informations">
+        <div className="title-section">
+          <h2> {logementActive.title}</h2>
+          <h3>{logementActive.location}</h3>
+          <div className="logements-tags">
+            {logementActive.tags.map((tag, index) => (
+              <span key={index}> {tag} </span>
+            ))}
+          </div>
+        </div>
+        <div className="host-informations">
+          <h4>{logementActive.host.name} </h4>
+          <img className="host-picture" src={logementActive.host.picture} />
+          <div className="rating">
+            {number.map((num, index) => (
+              <img
+                key={index}
+                src={num <= logementActive.rating ? starR : starW}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="collapse-logement">
+        <Collapse title="description" text={logementActive.description} />
+        <Collapse title="Equipements" text={logementActive.equipments} />
+      </div>
     </div>
   );
 }
